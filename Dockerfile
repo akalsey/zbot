1a0a13dd6c25673246407dc2a51ae181bb2b370f
Adam Kalsey <adam@kalsey.com>
2016-12-31 11:30:43 -0800

    Working bot implementation Use docker-compose to launch API Lighter weight dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:latest
+
+ENV DEBIAN_FRONTEND noninteractive
+ARG docker_port=3000
+ENV PORT ${docker_port}
+ENV NODE_PATH /root
+
+EXPOSE ${PORT}
+
+RUN useradd --user-group --create-home --shell /bin/false app
+COPY package.json npm-shrinkwrap.json $NODE_PATH/
+RUN chown -R app:app $NODE_PATH
+USER app
+WORKDIR $NODE_PATH
+RUN npm install
+
+COPY index.js $NODE_PATH/
+
+CMD ["node","/root/index.js"]
--- index.js
+++ index.js
@@ -6,14 +6,28 @@ var troposdk = require('tropo-webapi');
 var util = require('util')
 var Promise = require("bluebird");
 var ciscospark = require("ciscospark");
+const winston = require('winston');
 
 var app = express();
 
-var port = 5432;
+var port = process.env.PORT || 5432;
 
-var zmachine = 'http://localhost:'+port+'/zmachine/';
+winston.level = process.env.LOG_LEVEL || 'warn';
 
-app.use('/zmachine', require('../zmachine-api/src/app'));
+var zmachine = process.env.APIHOST;
+
+var sparkbotself = '';
+
+// Who am I? Allows me to ignore Spark messages sent by myself
+ciscospark.people.get('me')
+  .then(me => {
+    winston.log('debug', 'My Spark data is ', me);
+    sparkbotself = me.id;
+  })
+  .catch(function (err) {
+    winston.log('error', "I don't know who I am on Spark.", err);
+  });
+
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 
@@ -21,54 +35,110 @@ app.use(bodyParser.json()); // to support JSON-encoded bodies
 // a different game.
 // Set your Tropo app to WebAPI with an app URL of <your-server>/tropo
 app.post('/tropo', function(req, res) {
+  winston.log('silly', 'Incoming Tropo message', req.body);
     if (req.body.session.to.channel == 'TEXT') {
       var message = req.body.session.initialText;
       var session = req.body.session.from.e164Id;
 
       performAction(message, session)
         .then(reply => {
+          winston.log('debug', 'Tropo reply', {reply: reply, session: session});
           var tropo = new troposdk.TropoWebAPI();
           tropo.say(reply);
-          res.send(troposdk.TropoJSON(tropo));
+          var tropoML = troposdk.TropoJSON(tropo);
+          winston.log('silly', 'Sending TropoML', tropoML);
+          res.send(tropoML);
         })
         .catch(function (err) {
-            console.log("Tropo action failed " + err);
+          winston.log('error', 'Tropo action failed', err);
         });
     } else {
+      winston.log('debug', 'Rejecting voice call');
       tropo.reject();
-      res.send(troposdk.TropoJSON(tropo));
+      var tropoML = troposdk.TropoJSON(tropo);
+      winston.log('silly', 'Sending TropoML', tropoML);
+      res.send(tropoML);
     }
 });
 
 // A Cisco Spark message webhook listener. Each Spark room is treated as
 // a different game.
-// Create a Message webhook as your bot with a URL of <your-server>/spark and
-// no room filter
+// Create a Message webhook as your bot with a URL of <your-server>/spark
+// an action of 'create' and no room filter
 // Set the env variable CISCOSPARK_ACCESS_TOKEN to your bot's access token
 app.post('/spark', function(req, res) {
+  winston.log('debug', 'Spark wehook', req.body);
+
+    if (req.body.data.personId == sparkbotself) {
+      winston.log('silly', "It's my own message.");
+      return;
+    }
+
     var session = req.body.data.roomId;
 
     ciscospark.messages.get(req.body.data.id)
       .then(message => {
-        return performAction(message.text, session);
+        winston.log('debug', 'Spark message content', message);
+        // strip the mention & HTML from the message
+        var pattern = new RegExp('<spark-mention[^>]*data-object-id="' + sparkbotself + '"[^>]*>[^<]*<\/spark-mention>');
+        var action = message.html.replace(pattern,'');
+        action = action.replace(/<[^>]*>/g,'')
+        return performAction(action, session);
       })
       .then(reply => {
-        var md = reply.replace(/\n/, "`\n");
-        md = "`" + md;
-        md = md.replace(/\n/g, "<br/>");
-        res.send(md);
-        return ciscospark.messages.create({
-          markdown: md,
-          text: reply,
-          roomId: session
-        });
+        res.send(reply);
+        return sparkmessage(reply, session);
       })
       .catch(function (err) {
-          console.log("Spark Action failed " + err);
+        winston.log('error', 'Spark action failed', err);
       });
 });
 
+// A Cisco Spark membership webhook listener. When the bot is added to a room,
+// it will start a game.
+// Create a Membership webhook as your bot with a URL of <your-server>/sparkroom
+// an event of 'created' and a filter of personEmail = adventure@sparkbot.io
+// Set the env variable CISCOSPARK_ACCESS_TOKEN to your bot's access token
+app.post('/sparkroom', function(req, res) {
+  winston.log('debug', 'Spark wehook', req.body);
+
+    var session = req.body.data.roomId;
+    var create = {
+      method: 'POST',
+      uri: zmachine + 'games/',
+      json: true
+    };
+
+    create.body = { game: 'zork', label: session };
+    return rp(create)
+      .then(response => {
+        reply = response.data
+        winston.log('silly', 'zmachine reply', reply);
+        return sparkmessage(reply, session);
+      })
+      .catch(function (err) {
+        winston.log('error', 'Something went wrong', err);
+        return sparkmessage("Something went very wrong. It's not you, it's me.", session);
+      });
+});
+
+function sparkmessage(text, room) {
+  var md = text.replace(/\n/, "`\n");
+  md = "`" + md;
+  md = md.replace(/\n/g, "<br/>");
+  var message = {
+    markdown: md,
+    text: text,
+    roomId: room
+  };
+  winston.log('debug', 'Sending message to Spark', message);
+  return ciscospark.messages.create(message);
+}
+
 function performAction(action, session) {
+  action = action.trim();
+  winston.log('silly', 'action requested', action);
+
   var find = {
     method: 'GET',
     uri: zmachine + 'games/',
@@ -91,25 +161,40 @@ function performAction(action, session) {
 
   return rp(find)
     .then(body => {
+      winston.log('silly', 'games running ', body);
       var game = body.find(o => o.label === session);
       if (undefined != game && game.pid) {
+        winston.log('silly', 'Existing game found', game);
         // there is a game already, send the action to it
+        switch (action.toLowerCase()) {
+          case 'quit':
+          case 'save':
+          case 'restore':
+          case 'restart':
+          case 'script':
+          case 'unscript':
+            return {data: "Sorry, I can't do that."};
+          case '/game':
+            return {data: "Not yet."};
+        }
         act.uri = zmachine + 'games/' + game.pid + '/action';
         act.body = {action: action};
         return rp(act);
       } else {
         // start a new game & return the intro
+        winston.log('silly', 'No game found, starting new one.', body);
         create.body = { game: 'zork', label: session };
         return rp(create);
       }
     })
     .then(response => {
       reply = response.data
-      console.log(reply);
+      winston.log('silly', 'zmachine reply', reply);
       return reply;
     })
     .catch(function (err) {
-        console.log("something went wrong " + err);
+      winston.log('error', 'Something went wrong', err);
+      return "Something went very wrong. It's not you, it's me."
     });
 }
 
@@ -117,5 +202,5 @@ var server = app.listen(port, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('listening on %s:%s', host, port);
+    winston.log('info', 'listening', {host: host, port: port});
 });
